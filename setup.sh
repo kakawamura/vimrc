76e382538c841fe3c91aadb62fae57cff7603268
Kazushi Kawamura <wakaka2277@gmail.com>
2015-07-16 20:45:28 +0900

    add scripting for changing the colorscheme

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@ ln -s ~/vimrc/.vimrc ~/.vimrc
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 git clone https://www.github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+mkdir ~/.vim/colors
+git clone https://github.com/tomasr/molokai ~/.vim
+mv ~/.vim/molokai/colors/molokai.vim ~/.vim/colors
+rm -rf ~/.vim/molokai
